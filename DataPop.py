5da793804593eaa53538b1f9eacb59ee3077e78d
alanpadillachua <axp141330@utdallas.edu>
2017-03-14 14:54:38 -0500

    cleaning

--- DataPop.py
+++ DataPop.py
@@ -1,15 +1,16 @@
 import urllib.request
 from bs4 import BeautifulSoup
-
 def parsejedi(htmltxt):
     htmltxt
     num = 0
     print("Jedi Generals Parsed " + num)
     return
+
 with urllib.request.urlopen("http://starwars.wikia.com/wiki/Category:Jedi_Commanders") as url:
     r = url.read()
 soup = BeautifulSoup(r, "html.parser")
 jediPage = soup.get_text()
+
 #parsejedi(jediPage)
 #print(soup.prettify()[1102:1618]) #prints based on some section
 #print(soup.find_all('a')) # should print anything under the tag
