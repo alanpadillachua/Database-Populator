886b0ec6739cb212e12bd72dde69516833234afc
alanpadillachua <axp141330@utdallas.edu>
2017-03-14 11:43:17 -0500

    building framework

--- DataPop.py
+++ DataPop.py
@@ -3,8 +3,16 @@ import urllib.request
 with urllib.request.urlopen("http://starwars.wikia.com/wiki/Category:Jedi_Commanders") as url:
     r = url.read()
 soup = BeautifulSoup(r, "html.parser")
+jediPage = soup.get_text()
+parsejedi(jediPage)
+#print(soup.prettify()[1102:1618]) #prints based on some section
+#print(soup.find_all('a')) # should print anything under the tag
+#soup.prettify() #prints the whole html page
+#print(soup.get_text()) # just prints the text
 
-print(soup.prettify())
-
-
+def parsejedi(htmltxt):
+    htmltxt
+    num = 0
+    print("Jedi Generals Parsed " + num)
+    return
 
