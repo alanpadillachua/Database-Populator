bdfaada10dbb83e34dee5cabcb61247d472df0a2
alanpadillachua <axp141330@utdallas.edu>
2017-03-14 18:19:12 -0500

    switched ide testing changes

--- DataPop.py
+++ DataPop.py
@@ -1,9 +1,20 @@
+#################################################################
+# Project By: Alan Padilla Chua
+# This script basically takes in pages from the starwars wiki
+# gets the names and will eventually populate a database
+# with the names of Jedi, Clones, Driods, And Other Characters
+# eventually I want to use these names to create a name generator
+#################################################################
 import urllib.request
+
 from bs4 import BeautifulSoup
+
+
 def parsejedi(htmltxt):
-    htmltxt
+    htmltxt.split(" ")
     num = 0
-    print("Jedi Generals Parsed " + num)
+    print("Jedi Generals Parsed " + str(num))
+    num += 1
     return
 
 with urllib.request.urlopen("http://starwars.wikia.com/wiki/Category:Jedi_Commanders") as url:
@@ -11,11 +22,12 @@ with urllib.request.urlopen("http://starwars.wikia.com/wiki/Category:Jedi_Comman
 soup = BeautifulSoup(r, "html.parser")
 jediPage = soup.get_text()
 
+
 #parsejedi(jediPage)
 #print(soup.prettify()[1102:1618]) #prints based on some section
 #print(soup.find_all('a')) # should print anything under the tag
 #soup.prettify() #prints the whole html page
-print(soup.get_text()) # just prints the text
+#print(soup.get_text()) # just prints the text
 
 
 
